a733b3d0e08b448dd6892a8b11083bf9ddafe50d
okanishi <ok.man.sence@gmail.com>
2014-02-24 23:05:44 +0900

    Heroku Test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YOURApp::Application.config.secret_token = '20b2d9afcf735e888285c3581ab4e8c58abe3219636c84e84c93b00c32ee60f93398676fb3932d27ed0970e142febe507f1648b51e4a16a00891d38633adb789'
